9065620d6e48c7e21a9cba0da91a665a3b1e18b3
Matej <ruzicka.maty@gmail.com>
2024-12-31 14:46:53 +0100

    initial release

---
+++ fabric/src/main/java/com/mars/netherwartblock/Netherwartblock.java
@@ -0,0 +1,10 @@
+package com.mars.netherwartblock;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Netherwartblock implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+    }
+}
