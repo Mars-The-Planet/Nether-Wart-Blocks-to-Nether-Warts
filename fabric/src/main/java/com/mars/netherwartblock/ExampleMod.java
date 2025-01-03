987a70dbb2304d20b5533151a14daaacb4cdb85f
Matej <ruzicka.maty@gmail.com>
2025-01-03 15:32:49 +0100

    first

---
+++ fabric/src/main/java/com/mars/netherwartblock/ExampleMod.java
@@ -0,0 +1,11 @@
+package com.mars.netherwartblock;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+    }
+}
