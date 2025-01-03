df88929f744c05bd4132f50bcf31a8474847e640
Matej <ruzicka.maty@gmail.com>
2025-01-03 01:50:18 +0100

    first

---
+++ common/src/main/java/com/mars/netherwartblock/NetherWartConfig.java
@@ -0,0 +1,7 @@
+package com.mars.netherwartblock;
+
+import com.mars.deimos.config.DeimosConfig;
+
+public class NetherWartConfig extends DeimosConfig {
+    @Entry public static int wartAmount = 9;
+}
