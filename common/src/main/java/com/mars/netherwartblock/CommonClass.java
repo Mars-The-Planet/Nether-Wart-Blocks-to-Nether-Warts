c27eef525a42b8cf55d2d9aa7b1398f9ce26f2e5
Mars <ruzicka.maty@gmail.com>
2026-01-23 22:00:07 +0100

    updated to 1.21.11

--- common/src/main/java/com/mars/netherwartblock/CommonClass.java
+++ common/src/main/java/com/mars/netherwartblock/CommonClass.java
@@ -2,14 +2,11 @@ package com.mars.netherwartblock;
 
 import com.mars.deimos.config.DeimosConfig;
 import com.mars.deimos.datagen.DeimosRecipeGenerator;
-import com.mars.netherwartblock.platform.Services;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Items;
+import net.minecraft.resources.Identifier;
 
 public class CommonClass {
     public static void init() {
         DeimosConfig.init("netherwartblock", NetherWartConfig.class);
-        DeimosRecipeGenerator.createItemConvertorJson(ResourceLocation.withDefaultNamespace("nether_wart_block"), ResourceLocation.withDefaultNamespace("nether_wart"), NetherWartConfig.wartAmount);
+        DeimosRecipeGenerator.createItemConvertorJson(Identifier.withDefaultNamespace("nether_wart_block"), Identifier.withDefaultNamespace("nether_wart"), NetherWartConfig.wartAmount);
     }
 }
